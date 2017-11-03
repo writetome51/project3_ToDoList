21b635c9930ab6977bbb55c5e99414435bc2518f
Steve Thompson <writetome51@gmail.com>
2017-11-03 14:52:04 -0600

    more changes to css

--- jasmine/spec/StorageSpec.js
+++ jasmine/spec/StorageSpec.js
@@ -17,4 +17,21 @@ describe('Storage', function(){
             expect(storage.getCurrentList()).toEqual(list);
         });
     });
+
+    describe('saveLists', function(){
+
+        it('should save the given lists', function(){
+            let lists = [
+                {name:'listy', tasks:[]},
+                {name:'listo', tasks:[]}
+            ];
+
+            storage.saveLists(lists);
+
+            expect(storage.getLists()).toEqual(lists);
+
+           // expect(typeof storage.getLists()).toEqual('object');
+        });
+    });
+
 });
--- jasmine/src/Storage.js
+++ jasmine/src/Storage.js
@@ -1,16 +1,24 @@
 
 class Storage{
+
     constructor(){
         this.CURRENT_LIST_KEY = 'currentList';
-      //  this.list = {};
+        this.LISTS_KEY = 'lists';
     }
 
     saveCurrentList(list){
         localStorage.setItem(this.CURRENT_LIST_KEY, JSON.stringify(list));
-        this.list = list;
     }
 
     getCurrentList(){
         return JSON.parse( localStorage.getItem(this.CURRENT_LIST_KEY) );
+    }
+
+    saveLists(lists){
+        localStorage.setItem('lists', JSON.stringify(lists));
+    }
+
+    getLists(){
+        return JSON.parse(localStorage.getItem(this.LISTS_KEY));
     }
 }
