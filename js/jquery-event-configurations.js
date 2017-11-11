c588b5014a6d043cd2f2e596e081739fc4933300
Steve Thompson <writetome51@gmail.com>
2017-11-10 17:07:34 -0700

    created utilities.js file and made more changes to jquery-event-configurations.js

--- js/jquery-event-configurations.js
+++ js/jquery-event-configurations.js
@@ -1,30 +1,25 @@
 
-var clickedClass = 'list-item-clicked';
-var hoveredClass = 'list-item-hovered';
-var highlightClass = 'highlight-area';
-var itemTextClass = 'item-text';
-var removeGlyphiconClass = 'glyphicon-remove-circle';
-var pencilGlyphiconClass = 'glyphicon-pencil';
-var itemCheckboxClass = 'todo-checkbox';
-var invisibleClass = 'invisible';
-var itemText = $('.' + itemTextClass);
-var todoListItem = '';
-var todoCheckbox = $('.' + itemCheckboxClass);
-var removeGlyph = $('.' + removeGlyphiconClass);
-var clickedItem = false;
-
 
 itemText.click(function(){
     if (listItemIsAlreadyClicked($(this))){
-        undoClickedItem();
-    }
-    if ( thisItemIsAlreadyClicked(todoListItem) ){
         undoClickedItem();
     }
     else{
          makeThisItemTheClickedItem($(this));
     }
 });
+
+
+itemText.hover(
+    function onMouseOver(){
+        if (listItemIsNotClicked($(this))){
+            addHoveredClassToListItem($(this));
+        }
+    },
+    function onMouseOut(){
+        removeHoveredClassFromListItem($(this))
+    }
+);
 
 
 removeGlyph.click(function(){
@@ -43,116 +38,4 @@ todoCheckbox.click(function(){
 });
 
 
-itemText.hover(
-    function onMouseOver(){
-        if ( !$(this).hasClass(clickedClass) ){
-            $(this).addClass(hoveredClass);
-        }
-    },
-    function onMouseOut(){
-        $(this).removeClass(hoveredClass);
-    }
-);
 
-
-
-// Below are utility functions used in the events above.
-
-function undoClickedItem(){
-    if (thereIsAClickedItem()){
-        clickedItem.removeClass(clickedClass);
-        makeRemoveGlyphInvisible(clickedItem);
-        makePencilGlyphInvisible(clickedItem);
-        makeItemNotEditable(clickedItem);
-    }
-    clickedItem = false;
-}
-
-
-function makeThisItemTheClickedItem(obj){
-    undoClickedItem();
-    setClickedItem(obj);
-}
-
-
-function setClickedItem(obj){
-    clickedItem = obj;
-    removeHoverClassAndAddClickedClass(clickedItem);
-    makeRemoveGlyphVisible(clickedItem);
-    makePencilGlyphVisible(clickedItem);
-    makeItemEditable(clickedItem);
-}
-
-
-function makeRemoveGlyphVisible(obj){
-    obj.find('.' + removeGlyphiconClass).removeClass(invisibleClass);
-}
-
-function makeRemoveGlyphInvisible(obj){
-    obj.find('.' + removeGlyphiconClass).addClass(invisibleClass);
-}
-
-
-function makePencilGlyphInvisible(obj){
-    obj.find('.' + pencilGlyphiconClass).addClass(invisibleClass);
-}
-
-
-function makePencilGlyphVisible(obj){
-    obj.find('.' + pencilGlyphiconClass).removeClass(invisibleClass);
-}
-
-
-function thereIsAClickedItem(){
-    if (clickedItem){ return true; }
-    else { return false; }
-}
-
-
-function listItemIsAlreadyClicked(obj){
-    todoListItem = obj.parent('.' + highlightClass);
-    return thisItemIsAlreadyClicked(todoListItem);
-}
-
-
-function thisItemIsAlreadyClicked(obj){
-    return  obj.hasClass(clickedClass);
-}
-
-
-function removeHoverClassAndAddClickedClass(obj){
-    obj.addClass(clickedClass);
-    obj.removeClass(hoveredClass);
-}
-
-
-function makeItemEditable(obj){
-    obj.children('.' + itemTextClass).attr("contenteditable", "true");
-}
-
-
-function makeItemNotEditable(obj){
-    obj.children('.' + itemTextClass).removeAttr('contenteditable');
-}
-
-
-function clickedItemCheckboxIsChecked(){
-    return clickedItem.children('.' + itemCheckboxClass).prop('checked');
-}
-
-
-function removeListItem(item){
-    item.remove();
-}
-
-
-function createAccount(){}
-
-
-function login(){}
-
-
-function newList(){}
-
-
-function editLists(){}
---
+++ js/utilities.js
@@ -0,0 +1,131 @@
+
+var clickedClass = 'list-item-clicked';
+var hoveredClass = 'list-item-hovered';
+var highlightClass = 'highlight-area';
+var itemTextClass = 'item-text';
+var removeGlyphiconClass = 'glyphicon-remove-circle';
+var pencilGlyphiconClass = 'glyphicon-pencil';
+var itemCheckboxClass = 'todo-checkbox';
+var invisibleClass = 'invisible';
+var itemText = $('.' + itemTextClass);
+var todoListItem = '';
+var todoCheckbox = $('.' + itemCheckboxClass);
+var removeGlyph = $('.' + removeGlyphiconClass);
+var clickedItem = false;
+
+
+function undoClickedItem(){
+    if (thereIsAClickedItem()){
+        clickedItem.removeClass(clickedClass);
+        makeRemoveGlyphInvisible(clickedItem);
+        makePencilGlyphInvisible(clickedItem);
+        makeItemNotEditable(clickedItem);
+    }
+    clickedItem = false;
+}
+
+
+function makeThisItemTheClickedItem(obj){
+    undoClickedItem();
+    setClickedItem(obj);
+}
+
+
+function setClickedItem(obj){
+    clickedItem = obj;
+    removeHoverClassAndAddClickedClass(clickedItem);
+    makeRemoveGlyphVisible(clickedItem);
+    makePencilGlyphVisible(clickedItem);
+    makeItemEditable(clickedItem);
+}
+
+
+function makeRemoveGlyphVisible(obj){
+    obj.find('.' + removeGlyphiconClass).removeClass(invisibleClass);
+}
+
+function makeRemoveGlyphInvisible(obj){
+    obj.find('.' + removeGlyphiconClass).addClass(invisibleClass);
+}
+
+
+function makePencilGlyphInvisible(obj){
+    obj.find('.' + pencilGlyphiconClass).addClass(invisibleClass);
+}
+
+
+function makePencilGlyphVisible(obj){
+    obj.find('.' + pencilGlyphiconClass).removeClass(invisibleClass);
+}
+
+
+function thereIsAClickedItem(){
+    if (clickedItem){ return true; }
+    else { return false; }
+}
+
+
+function listItemIsAlreadyClicked(obj){
+    todoListItem = obj.parent('.' + highlightClass);
+    return thisItemIsAlreadyClicked(todoListItem);
+}
+
+
+function listItemIsNotClicked(obj){
+    return  ( ! listItemIsAlreadyClicked(obj));
+}
+
+
+function thisItemIsAlreadyClicked(obj){
+    return  obj.hasClass(clickedClass);
+}
+
+
+function removeHoverClassAndAddClickedClass(obj){
+    obj.addClass(clickedClass);
+    obj.removeClass(hoveredClass);
+}
+
+
+function addHoveredClassToListItem(obj){
+    todoListItem = obj.parent('.' + highlightClass);
+    todoListItem.addClass(hoveredClass);
+}
+
+
+function removeHoveredClassFromListItem(obj){
+    todoListItem = obj.parent('.' + highlightClass);
+    todoListItem.removeClass(hoveredClass);
+}
+
+
+function makeItemEditable(obj){
+    obj.children('.' + itemTextClass).attr("contenteditable", "true");
+}
+
+
+function makeItemNotEditable(obj){
+    obj.children('.' + itemTextClass).removeAttr('contenteditable');
+}
+
+
+function clickedItemCheckboxIsChecked(){
+    return clickedItem.children('.' + itemCheckboxClass).prop('checked');
+}
+
+
+function removeListItem(item){
+    item.remove();
+}
+
+
+function createAccount(){}
+
+
+function login(){}
+
+
+function newList(){}
+
+
+function editLists(){}
